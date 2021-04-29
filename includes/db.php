36336083742b6c8dec20bcd5813cfaeb744d12f2
AayushMakwana <79373249+AayushMakwana@users.noreply.github.com>
2021-04-29 13:46:33 +0530

    I changes for DB Connectins

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,21 @@
 <?php
 
-	$dsn = "mysql:host=localhost;dbname=techbarik";
+	// In localhost
+	// $dsn = "mysql:host=localhost;dbname=techbarik";
+
+	// try {
+	// 	$pdo = new PDO($dsn, 'root', '');
+	// }
+
+
+
+
+	// In Checking Remote DB Connection
+
+	$dsn = "mysql:host=remotemysql.com;dbname=hRLfKonC1X";
 
 	try {
-		$pdo = new PDO($dsn, 'root', '');
+		$pdo = new PDO($dsn, 'hRLfKonC1X', 'CRMFJqs9MK');
 	}
 	catch(PDOException $e) {
 		echo $e->getMessage();
